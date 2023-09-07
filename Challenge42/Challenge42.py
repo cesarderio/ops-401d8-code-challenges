f1075a6def3450970eb90232d90150a1d5a4c7b0
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-09-07 09:19:39 -0700

    working on challenge 42

--- Challenge42/Challenge42.py
+++ Challenge42/Challenge42.py
@@ -34,7 +34,7 @@ if resp == '1':
     print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
 elif resp == '2':
     print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, port_range, '-v -sU')
+    scanner.scan(ip_addr, range, '-v -sU')
     print(scanner.scaninfo())
     print("Ip Status: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
@@ -44,7 +44,7 @@ elif resp == '2':
         print("No UDP ports found open.")
 elif resp == '3':
     print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, port_range, '-v -sS -sV -sC -A -O')
+    scanner.scan(ip_addr, range, '-v -sS -sV -sC -A -O')
     print(scanner.scaninfo())
     print("Ip Status: ", scanner[ip_addr].state())
     print(scanner[ip_addr].all_protocols())
