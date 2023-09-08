c35b15ebe075d945b45f26f8138a7b766f395b68
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-09-08 16:15:12 -0700

    working on code challenge 44

---
+++ Challenge44/Challenge44.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+# Script Name:                  Attack Tools Part 2 of 3
+# Author:                       Raphael Chookagian
+# Date of latest revision:      09/06/2023
+# Purpose:                      Create a python script: Complete the Demo.
+
+
+import socket
+
+# Create a socket object
+sockmod = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Set a timeout value
+# timeout = # TODO: Set a timeout value here.
+timeout = 1  # 1 second, you can adjust this as needed
+sockmod.settimeout(timeout)
+
+# Collect a host IP from the user
+# hostip = # TODO: Collect a host IP from the user.
+hostip = input("Enter the target IP address: ")
+
+# Collect a port number from the user, then convert it to an integer data type
+# portno = # TODO: Collect a port number from the user, then convert it to an integer data type.
+portno = int(input("Enter the target port number: "))
+
+# def portScanner(portno):
+#     if sockmod.FUNCTION((hostip, portno)): # TODO: Replace "FUNCTION" with the appropriate socket.function call as found in the [socket docs](https://docs.python.org/3/library/socket.html)
+#         print("Port closed")
+#     else:
+#         print("Port open")
+
+def portScanner(port):
+    try:
+        # Connect to the target IP and port
+        sockmod.connect((hostip, port))
+        print("Port open")
+    except socket.error:
+        print("Port closed")
+    finally:
+        sockmod.close()
+
+portScanner(portno)
---
+++ Challenge44/README.md
@@ -0,0 +1,26 @@
+# Ops Challenge - Create a Port Scanner
+
+[Demo Code](https://codefellows.github.io/ops-401-cybersecurity-guide/curriculum/class-44/challenges/DEMO.html)
+
+Copy this template down to your local coding environment and finish all TODOs.
+
+The purpose of this script is to determine if a target port is open or closed, using strictly Python 3 commands. To do so, we’ll be importing the socket module, a low-level networking interface for Python.
+
+```#!/usr/bin/python3
+
+import socket
+
+sockmod = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+timeout = # TODO: Set a timeout value here.
+sockmod.settimeout(timeout)
+
+hostip = # TODO: Collect a host IP from the user.
+portno = # TODO: Collect a port number from the user, then convert it to an integer data type.
+
+def portScanner(portno):
+    if sockmod.FUNCTION((hostip, portno)): # TODO: Replace "FUNCTION" with the appropriate socket.function call as found in the [socket docs](https://docs.python.org/3/library/socket.html)
+        print("Port closed")
+    else:
+        print("Port open")
+
+portScanner(port)```
